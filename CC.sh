372d4fd22c036d67d6af8428e22cdd5754f6f746
ａｎｘｉｏｕｓXａｄｎａｎ <95622276+ULTRA-ADNAN-404@users.noreply.github.com>
2023-08-22 18:36:45 +0600

    Add files via upload

---
+++ CC.sh
@@ -0,0 +1,57 @@
+clear
+figlet CC Generator Adnan
+echo "Version : 1.0"
+echo
+echo "<=================================>"
+echo "==== > Credit Card Generator < ===="
+echo "<=================================>"
+echo "Creator : ANXIOUSxADNAN"
+echo "My Team : TeamAnxious"
+echo "<=================================>"
+echo "=====>This Tools Random Card <====="
+echo "======> Login To Start Bro <====== "
+echo "<=================================>"
+echo
+echo "What Is Your Name Bro?"
+read Bro
+echo
+echo "Hi $Bro Welcome To My Tools. Tnx To Use For My Tools"
+echo
+echo "< ========================== >"
+echo "== > Press Enter To Start < =="
+echo "< ========================== >"
+read
+echo "Start!!!"
+echo
+echo "Name : Ethel C. Gonzales"
+echo "MasterCard : 5311 6135 4545 1794"
+echo "Expires : 10/2023"
+echo "CVC2 : 195"
+sleep 1
+echo
+echo "Name : Jeremy J. Smith"
+echo "Visa : 4532 4614 4478 7827"
+echo "Expires : 4/2026"
+echo "CVV2 : 764"
+sleep 2
+echo
+echo "Name : Marie T. Garner"
+echo "MasterCard : 5374 2575 2080 9875"
+echo "Expires : 4/2024"
+echo "CVC2 : 153"
+sleep 2
+echo
+echo "Name : Victoria E. Hurley"
+echo "MasterCard : 5401 8369 5085 6642"
+echo "Expires : 1/2027"
+echo "CVC2 : 581"
+sleep2 
+echo
+echo "Name : Charlotte S. Johnson"
+echo "Visa : 4556 7540 5543 4247"
+echo "Expires : 9/2027"
+echo "CCV2 : 481"
+echo
+sleep 2
+echo "Ty For Use :)"
+echo
